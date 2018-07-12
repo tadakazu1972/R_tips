15f58fc4344ffb88ffce87d8195e787024a8f98f
Ryusei Tabata <ryuseitabata@RyuseinoiMac.local>
2018-07-12 12:09:17 +0900

    全小学校プロット

--- 全小学校プロット.R
+++ 全小学校プロット.R
@@ -21,11 +21,14 @@ data0 <- read_csv("H28小学校標準化得点.csv")
 #得点データと緯度経度を結合
 data <- left_join(data0, master, by=c("小学校"="学校名"))
 
+#得点表示で使うため項目名を代入
+column <- colnames(data)
+
 #mexで境界線全体の拡大率を操作
 par(mex="0.3", family="HiraKakuProN-W3")
 
 #境界線描画
-plot(st_geometry(shape[1:24,1]), main=paste("平成28年度　", colnames(data)[3], "　標準化得点" , sep=""))
+plot(st_geometry(shape[1:24,1]), main=paste("平成28年度　", column[4], "　標準化得点" , sep=""))
 
 #小学校の色付け　点数を　赤黄青 色をkmeansで
 color <- data$`3年生/算数` %>% classIntervals(., 8, style="kmeans") %>% findColours(.,pal=brewer.pal(8,"RdYlBu"))
@@ -34,4 +37,4 @@ color <- data$`3年生/算数` %>% classIntervals(., 8, style="kmeans") %>% find
 points(data$X, data$Y, ps=24, pch=16, col=color)
 
 #点数をY座標ずらして描画
-text(data$X, data$Y-0.0024, labels=data$`3年生/算数`, cex=0.5)
+text(data$X, data$Y-0.0024, labels=data[[column[4]]], cex=0.5)
