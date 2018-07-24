68d66c2f7136e018d2ceef7ff5468f6a22ff05dd
tadakazu1972 <tadakazu1972@gmail.com>
2018-07-24 22:36:29 +0900

    write.csv

--- CSVデータ大量処理.R
+++ CSVデータ大量処理.R
@@ -14,3 +14,6 @@ column <- colnames(data)
 
 #学校別、学年別、アンケート全項目平均（学校、学年で２つカラムが減るので7:41）
 data1 <- data %>% group_by(学校名,学年) %>% summarise_at(c(7:41),mean)
+
+#csv書き出し 標準はutf-8になるので、あとでExcelで読めるようにCP932でエンコーディング
+write.csv(data1, "xxxx.csv", fileEncoding="CP932")
