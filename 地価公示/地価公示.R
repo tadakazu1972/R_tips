9ed61c2b9ed2d3a526b220ff24a210a870fa9343
tadakazu1972 <tadakazu1972@gmail.com>
2019-09-29 20:33:37 +0900

    地価公示　鉄道

--- 地価公示/地価公示.R
+++ 地価公示/地価公示.R
@@ -4,30 +4,19 @@ library(readr)
 library(RColorBrewer)
 library(classInt)
 
+#Mac　作業フォルダ設定
 setwd("~/Desktop/地価公示/")
 
-#まずは下図を描く
-#国勢調査小地域境界シェープファイル読込
-#eStatから取得 .dbf .prj .shxも合わせて作業ディレクトリ下部のshapeフォルダに入れておくべし
-shape0 <- st_read(dsn = "~/Desktop/地価公示/base/", layer = "N03-19_27_190101")
-
-#Windows WSLのフォルダパス
-#shape0 <- st_read(dsn = "/Users/tadakazu/AppData/Local/Packages/CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc/LocalState/rootfs/home/tadakazu/R_tips/地価公示/base", layer = "N03-19_27_190101")
-
-#タイトルラベルの設定
-name="大阪府"
-
-#境界　描画
-par(new=TRUE, mex="0.2", family="HiraKakuProN-W3", xpd=TRUE, xaxt="n")
+#グラフィックパラメータ
+par(mex="0.2", family="HiraKakuProN-W3", xpd=FALSE, xaxt="n")
 
 #Windows
-#par(new=TRUE, mex="0.2", xpd=TRUE, xaxt="n")
+#par(mex="0.2", xpd=FALSE, xaxt="n")
 #windowsFonts(MEI = windowsFont("Meiryo"))
 
-plot(st_geometry(shape0[,4]), xlim=c(135.0913,135.7466), ylim=c(34.27182,35.05129), main=paste(name, " ", "地価公示 (2019年)", sep=""))
-
-#次に代表地点の地価公示を描く
-#地価公示シェープファイル読込
+##########################################
+#地価公示　描画
+#地価公示　シェープファイル読込
 shape <- st_read(dsn = "~/Desktop/地価公示/H31/", layer = "L01-19_27")
 
 #windows H31/と最後に/を入れるとエラー
@@ -37,21 +26,36 @@ shape <- st_read(dsn = "~/Desktop/地価公示/H31/", layer = "L01-19_27")
 #Factor型を数値に変換するのだが、一度文字変換してからでないとカテゴリーを変換してしまう
 data = as.numeric(as.character(shape$L01_006))
 
-#カラム名取得
-column = colnames(data)
-
-#色
-#col_km <- data %>% classIntervals(., 8, style="kmeans") %>% findColours(.,pal=rev(brewer.pal(10,"Spectral")))
-#色　Spectralの色順序を逆で　fixedBreaks
+#公示価格　色設定　Spectralの色順序を逆で　fixedBreaks
 col_km <- data %>% classIntervals(., 11, style="fixed", fixedBreaks=c(0,5000,10000,30000,50000,100000,200000,300000,500000,1000000,max(.))) %>% findColours(.,pal=rev(brewer.pal(11,"Spectral")))
 
-#代表地点　描画 xlimとylimをshape0と合わせないとずれる
-par(new=TRUE, mex="0.2", family="HiraKakuProN-W3", xpd=TRUE, xaxt="n")
+#地価公示　描画 xlimとylimをshape0と合わせないとずれる
+par(new=T)
+plot(st_geometry(shape[,6]), pch=16, cex=1, col=col_km, xlim=c(135.0913,135.7466), ylim=c(34.27182,35.05129))
 
-#windows
-#par(new=TRUE, mex="0.2", xpd=TRUE, xaxt="n")
+############################################
+#鉄道　描画
+#鉄道　シェープファイル読み込み
+#Windows WSL
+#rails <- st_read(dsn = "/Users/tadakazu/AppData/Local/Packages/CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc/LocalState/rootfs/home/tadakazu/R_tips/地価公示/rails", layer = "N02-18_RailroadSection")
 
-plot(st_geometry(shape[,6]), pch=16, cex=0.8, col=col_km, xlim=c(135.0913,135.7466), ylim=c(34.27182,35.05129))
+#鉄道　描画
+par(new=T)
+plot(st_geometry(rails), xlim=c(135.0913,135.7466), ylim=c(34.27182,35.05129), lwd=1, lty="solid", col="blue", bg="transparent")
 
+############################################
+#行政区域　描画
+#行政区域　シェープファイル読込
+#国土数値情報から取得 .dbf .prj .shxも合わせて作業ディレクトリ下部フォルダに入れておくべし
+shape0 <- st_read(dsn = "~/Desktop/地価公示/base/", layer = "N03-19_27_190101")
+
+#Windows WSLの場合パス
+#shape0 <- st_read(dsn = "/Users/tadakazu/AppData/Local/Packages/CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc/LocalState/rootfs/home/tadakazu/R_tips/地価公示/base", layer = "N03-19_27_190101")
+
+#行政区域　描画
+par(new=T)
+plot(st_geometry(shape0[,4]), xlim=c(135.0913,135.7466), ylim=c(34.27182,35.05129), border="grey3", main=paste("大阪府　地価公示 (2019年)", sep=""))
+
+##########################################
 #凡例
-legend("topleft", legend=c("5,000円/㎡","10,000円/㎡","30,000円/㎡","50,000円/㎡","100,000円/㎡","200,000円/㎡","300,000円/㎡","500,000円/㎡","1,000,000円/㎡"), pch=c(16,16,16,16,16,16,16,16), lty=c(1,2,3,4,5,6,7,8), col=c("#5E4FA2","#3288BD","#66C2A5","#ABDDA4","#E6F598","#FEE08B","#FDAE61","#F46D43","#D53E4F","#9E0142"), cex=0.8)
+legend("topleft", legend=c("5,000円/㎡","10,000円/㎡","30,000円/㎡","50,000円/㎡","100,000円/㎡","200,000円/㎡","300,000円/㎡","500,000円/㎡","1,000,000円/㎡"), pch=c(16,16,16,16,16,16,16,16), lty=c(1,2,3,4,5,6,7,8), col=c("#5E4FA2","#3288BD","#66C2A5","#ABDDA4","#E6F598","#FEE08B","#FDAE61","#F46D43","#D53E4F","#9E0142"), cex=0.8, bg="white")
---
+++ 地価公示/地価公示_dark.R
@@ -0,0 +1,61 @@
+library(dplyr)
+library(sf)
+library(readr)
+library(RColorBrewer)
+library(classInt)
+
+#Mac　作業フォルダ設定
+setwd("~/Desktop/地価公示/")
+
+#グラフィックパラメータ
+par(mex="0.2", family="HiraKakuProN-W3", xpd=FALSE, xaxt="n", bg="black")
+
+#Windows
+#par(mex="0.2", xpd=FALSE, xaxt="n", bg="black")
+#windowsFonts(MEI = windowsFont("Meiryo"))
+
+##########################################
+#地価公示　描画
+#地価公示　シェープファイル読込
+shape <- st_read(dsn = "~/Desktop/地価公示/H31/", layer = "L01-19_27")
+
+#windows H31/と最後に/を入れるとエラー
+#shape <- st_read(dsn = "/Users/tadakazu/AppData/Local/Packages/CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc/LocalState/rootfs/home/tadakazu/R_tips/地価公示/H31", layer = "L01-19_27")
+
+#公示価格　標準地の地価.単位を「円/m2」とする。
+#Factor型を数値に変換するのだが、一度文字変換してからでないとカテゴリーを変換してしまう
+data = as.numeric(as.character(shape$L01_006))
+
+#公示価格　色設定　Spectralの色順序を逆で　fixedBreaks
+col_km <- data %>% classIntervals(., 11, style="fixed", fixedBreaks=c(0,5000,10000,30000,50000,100000,200000,300000,500000,1000000,max(.))) %>% findColours(.,pal=rev(brewer.pal(11,"Spectral")))
+
+#地価公示　描画 xlimとylimをshape0と合わせないとずれる
+par(new=T)
+plot(st_geometry(shape[,6]), pch=18, cex=1, col=col_km, xlim=c(135.0913,135.7466), ylim=c(34.27182,35.05129))
+
+############################################
+#鉄道　描画
+#鉄道　シェープファイル読み込み
+#Windows WSL
+#rails <- st_read(dsn = "/Users/tadakazu/AppData/Local/Packages/CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc/LocalState/rootfs/home/tadakazu/R_tips/地価公示/rails", layer = "N02-18_RailroadSection")
+
+#鉄道　描画
+par(new=T)
+plot(st_geometry(rails), xlim=c(135.0913,135.7466), ylim=c(34.27182,35.05129), lwd=1, lty="solid", col="purple", bg="transparent")
+
+############################################
+#行政区域　描画
+#行政区域　シェープファイル読込
+#国土数値情報から取得 .dbf .prj .shxも合わせて作業ディレクトリ下部フォルダに入れておくべし
+shape0 <- st_read(dsn = "~/Desktop/地価公示/base/", layer = "N03-19_27_190101")
+
+#Windows WSLの場合パス
+#shape0 <- st_read(dsn = "/Users/tadakazu/AppData/Local/Packages/CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc/LocalState/rootfs/home/tadakazu/R_tips/地価公示/base", layer = "N03-19_27_190101")
+
+#行政区域　描画
+par(new=T)
+plot(st_geometry(shape0[,4]), xlim=c(135.0913,135.7466), ylim=c(34.27182,35.05129), main=paste("大阪府　地価公示 (2019年)", sep=""), border="green4", bg="transparent")
+
+##########################################
+#凡例
+legend("topleft", legend=c("5,000円/㎡","10,000円/㎡","30,000円/㎡","50,000円/㎡","100,000円/㎡","200,000円/㎡","300,000円/㎡","500,000円/㎡","1,000,000円/㎡"), pch=c(16,16,16,16,16,16,16,16), lty=c(1,2,3,4,5,6,7,8), col=c("#5E4FA2","#3288BD","#66C2A5","#ABDDA4","#E6F598","#FEE08B","#FDAE61","#F46D43","#D53E4F","#9E0142"), cex=0.8, bg="white")
